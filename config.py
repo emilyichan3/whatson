0b09a0208c17a1330c15980f7cf88efe2aefff59
Yi-Jung Chen <emilyichan3@gmail.com>
2024-11-25 21:19:32 +0000

    Connected to prostgresql server in render

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
-SQLALCHEMY_DATABASE_URI = os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
-# SQLALCHEMY_DATABASE_   URI = "post   gresql://ucdtestdatabase_user:LILVLDIhZcSObq1Yd5ZK8sBKUl2pqRYN@dpg-cs8mj088fa8c73cf2cp0-a.oregon-postgres.render.com/ucdtestdatabase"
+# SQLALCHEMY_DATABASE_URI = os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
+SQLALCHEMY_DATABASE_URI = "postgresql://whatson_database_user:MvygQ3Xniwu9Cd7UDLJnIH7I4B0grIr8@dpg-ct2ehp56l47c73ban60g-a.oregon-postgres.render.com/whatson_database"
