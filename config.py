95de22bdd81346c3a75b0280521f4306440154ba
Yi-Jung Chen <emilyichan3@gmail.com>
2024-11-25 21:31:19 +0000

    Fixed the overflow in head section in the small screen

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
 # SQLALCHEMY_DATABASE_URI = os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
-SQLALCHEMY_DATABASE_URI = "postgresql://whatson_database_user:MvygQ3Xniwu9Cd7UDLJnIH7I4B0grIr8@dpg-ct2ehp56l47c73ban60g-a.oregon-postgres.render.com/whatson_database"
+SQLALCHEMY_DATABASE_URI = "External Database URL"
